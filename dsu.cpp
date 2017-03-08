7a21a45c32e84886f6b11d5d979e17e24fde40fe
glotovmikhail <glotov.mikhail@mail.ru>
2017-03-08 18:59:43 +0300

    shit

--- dsu.cpp
+++ dsu.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 DSU::DSU(int w, int h) {
     for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+        for (int j = 0; j < w; j++) {sadfasdgfasdfa
             parent[i][j] = make_pair(i, j);
         }
     }
